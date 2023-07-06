cdff694e1c6d736c326c13859c59957a6d60ebdd
germanfabriziogomez <gegomez@frba.utn.edu.ar>
2023-07-06 11:53:00 -0300

    Primer parte hecha en clase

---
+++ programa.pl
@@ -0,0 +1,20 @@
+
+canta(megurineLuka, cancion(nightFever, 4)).
+
+canta(megurineLuka, cancion(foreverYoung,5)).
+
+canta(hatsuneMiku,cancion(tellYourWorld,4)).
+
+canta(gumi,cancion(forveverYoung,4)).
+
+canta(gumi,cancion(tellYourWorld,5)).
+
+canta(seeU,cancion(novemberRain,6)).
+
+canta(seeU,cancion(nightFever,5)).
+
+/*Debido al principio de Universo Cerrado, no es necesario especificar que kaito 
+no sabe cantar ninguna cancion, ya que al no estar en la base de conocimientos,
+todo lo desconocido se toma como falso.
+*/
+
